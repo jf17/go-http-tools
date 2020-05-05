6abe2e521f53243fecb8f554f8797c857dcefc1a
Alex Alexandrovich <javafirst17@gmail.com>
2020-05-05 16:56:37 +0300

    Create httptools.go

---
+++ httptools.go
@@ -0,0 +1,20 @@
+package httptools
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+
+func GetBody(url string) string  {
+	client := http.Client{}
+	resp, err := client.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	out, err := ioutil.ReadAll(resp.Body)
+	return string(out)
+}
+
